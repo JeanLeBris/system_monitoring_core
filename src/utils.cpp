46156ead94ce401b1370a073eb7053914550f62a
Jean Le Bris <jean.le.bris.29850@gmail.com>
2025-10-17 11:51:06 +0200

    podify the get_ip_addresses() function

--- src/utils.cpp
+++ src/utils.cpp
@@ -65,37 +65,39 @@ long long get_stoll(std::string value){
 std::vector<std::string> get_ip_addresses(std::string path){
     std::vector<std::string> output;
 
-    FILE *file = fopen(path.c_str(), "r");
-    if (file == NULL) {
-        printf("Cannot open file %s\n", path.c_str());
-        exit(0);
-    }
-    
-    char buffer_char_array[1000] = "\0";
-    fread(buffer_char_array, sizeof(buffer_char_array[0]), 999, file);
-
-    std::string buffer_string;
-    buffer_string.assign(buffer_char_array);
-    
-    std::string::const_iterator index = buffer_string.cbegin();
-    std::string::const_iterator end = buffer_string.cend();
-    int buffer_int = 1;
-
-    while(index != end){
-        if(*index.base() == 13 || *index.base() == 10){
-            buffer_int = 1;
+    if(path != ""){
+        FILE *file = fopen(path.c_str(), "r");
+        if (file == NULL) {
+            printf("Cannot open file %s\n", path.c_str());
+            exit(0);
         }
-        else{
-            if(buffer_int == 1){
-                output.push_back("");
-                buffer_int = 0;
+        
+        char buffer_char_array[1000] = "\0";
+        fread(buffer_char_array, sizeof(buffer_char_array[0]), 999, file);
+
+        std::string buffer_string;
+        buffer_string.assign(buffer_char_array);
+        
+        std::string::const_iterator index = buffer_string.cbegin();
+        std::string::const_iterator end = buffer_string.cend();
+        int buffer_int = 1;
+
+        while(index != end){
+            if(*index.base() == 13 || *index.base() == 10){
+                buffer_int = 1;
             }
-            output.at(output.size()-1).push_back(*index.base());
+            else{
+                if(buffer_int == 1){
+                    output.push_back("");
+                    buffer_int = 0;
+                }
+                output.at(output.size()-1).push_back(*index.base());
+            }
+            index++;
         }
-        index++;
-    }
 
-    fclose(file);
+        fclose(file);
+    }
 
     return output;
 }
