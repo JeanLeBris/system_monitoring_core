0859b13c6b82fefe3f047145092cb723ddbe828f
Jean Le Bris <jean.le.bris.29850@gmail.com>
2025-11-08 16:12:19 +0100

    switch the windows source files calls to wmic to the winapi, except for the physical drives

--- src/windows/monitoring.cpp
+++ src/windows/monitoring.cpp
@@ -1,72 +1,12 @@
 #include <iostream>
 #include <cmath>
+#include <windows.h>
 
 #include "monitoring.hpp"
 #include "output_converter.hpp"
 #include "utils.hpp"
 
 namespace monitoring{
-    // long long monitoring::System::get_cpu_load_percentage(){
-    //     std::string result;
-    //     char bar[4]{};
-
-    //     result = exec("wmic cpu get Loadpercentage");
-    //     // result.copy(bar, 3, 18);
-    //     result.copy(bar, 3, get_const_str_length("Loadpercentage    "));
-    //     if(bar[2] == ' ' || bar[2] == '\n'){
-    //         bar[2] = '\0';
-    //         if(bar[1] == ' ' || bar[1] == '\n'){
-    //             bar[1] = '\0';
-    //         }
-    //     }
-
-    //     return atoll(bar);
-    // }
-
-    // long long monitoring::System::get_total_ram(){
-    //     std::string result;
-    //     char bar[20]{};
-
-    //     result = exec("wmic computersystem get TotalPhysicalMemory");
-    //     // result.copy(bar, 19, 23);
-    //     result.copy(bar, 19, get_const_str_length("TotalPhysicalMemory    "));
-    //     if(bar[2] == ' ' || bar[2] == '\n'){
-    //         bar[2] = '\0';
-    //         if(bar[1] == ' ' || bar[1] == '\n'){
-    //             bar[1] = '\0';
-    //         }
-    //     }
-
-    //     return atoll(bar) / 1000;
-    //     // return atoll(bar);
-    // }
-
-    // long long monitoring::System::get_free_ram(){
-    //     std::string result;
-    //     char bar[20]{};
-
-    //     result = exec("wmic OS get FreePhysicalMemory");
-    //     // result.copy(bar, 19, 22);
-    //     result.copy(bar, 19, get_const_str_length("FreePhysicalMemory    "));
-    //     if(bar[2] == ' ' || bar[2] == '\n'){
-    //         bar[2] = '\0';
-    //         if(bar[1] == ' ' || bar[1] == '\n'){
-    //             bar[1] = '\0';
-    //         }
-    //     }
-
-    //     return atoll(bar);
-    // }
-
-    // long long monitoring::System::get_ram_load_percentage(){
-    //     long long total_ram = get_total_ram();
-    //     long long free_ram = get_free_ram();
-
-    //     long long used_ram = total_ram - free_ram;
-
-    //     return 100 * used_ram / total_ram;
-    // }
-
     void System::update_hostname(){
         converter::organized_data_array converted_result = converter::windows_hostname_converter(exec("hostname"));
 
@@ -76,90 +16,136 @@ namespace monitoring{
     }
 
     void System::update_basic_cpu_load_percentage(){
-        converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic cpu get LoadPercentage /VALUE"));
-        this->cpu.cpu_load_percentage = get_stoll(converter::get_value_from_key(converted_result.data[0], "LoadPercentage"));
-        free(converted_result.data);
+        FILETIME idleTime, kernelTime, userTime;
+        GetSystemTimes(&idleTime, &kernelTime, &userTime);
+        std::chrono::system_clock::time_point req_time = std::chrono::system_clock::now();
+        int64_t idle_tick, kernel_tick, user_tick, total_tick, activity_tick;
+        idle_tick = (((unsigned long long)(idleTime.dwHighDateTime))<<32)|((unsigned long long)idleTime.dwLowDateTime);
+        kernel_tick = (((unsigned long long)(kernelTime.dwHighDateTime))<<32)|((unsigned long long)kernelTime.dwLowDateTime);
+        user_tick = (((unsigned long long)(userTime.dwHighDateTime))<<32)|((unsigned long long)userTime.dwLowDateTime);
+        total_tick = kernel_tick + user_tick;
+        activity_tick = total_tick - idle_tick;
+
+        this->cpu.time = req_time;
+        this->cpu.last_time = this->cpu.time - std::chrono::seconds{1};
+        this->cpu.cpu_activity = activity_tick;
+        this->cpu.cpu_idle = idle_tick;
+        this->cpu.last_cpu_activity = this->cpu.cpu_activity - 1;
+        this->cpu.last_cpu_idle = this->cpu.cpu_idle - 1;
+        this->cpu.cpu_load_percentage = 0 * (this->cpu.cpu_activity - this->cpu.last_cpu_activity) / (this->cpu.cpu_idle - this->cpu.last_cpu_idle);
     }
 
     void System::update_cpu_load_percentage(){
-        converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic cpu get LoadPercentage /VALUE"));
-        this->cpu.cpu_load_percentage = get_stoll(converter::get_value_from_key(converted_result.data[0], "LoadPercentage"));
-        free(converted_result.data);
+        FILETIME idleTime, kernelTime, userTime;
+        GetSystemTimes(&idleTime, &kernelTime, &userTime);
+        std::chrono::system_clock::time_point req_time = std::chrono::system_clock::now();
+        int64_t idle_tick, kernel_tick, user_tick, total_tick, activity_tick;
+        idle_tick = (((unsigned long long)(idleTime.dwHighDateTime))<<32)|((unsigned long long)idleTime.dwLowDateTime);
+        kernel_tick = (((unsigned long long)(kernelTime.dwHighDateTime))<<32)|((unsigned long long)kernelTime.dwLowDateTime);
+        user_tick = (((unsigned long long)(userTime.dwHighDateTime))<<32)|((unsigned long long)userTime.dwLowDateTime);
+        total_tick = kernel_tick + user_tick;
+        activity_tick = total_tick - idle_tick;
+
+        this->cpu.last_time = this->cpu.time;
+        this->cpu.time = req_time;
+        this->cpu.last_cpu_activity = this->cpu.cpu_activity;
+        this->cpu.last_cpu_idle = this->cpu.cpu_idle;
+        this->cpu.cpu_activity = activity_tick;
+        this->cpu.cpu_idle = idle_tick;
+        this->cpu.cpu_load_percentage = 100 * (this->cpu.cpu_activity - this->cpu.last_cpu_activity) / (this->cpu.cpu_idle - this->cpu.last_cpu_idle);
     }
 
     void System::update_total_ram(){
-        converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic computersystem get TotalPhysicalMemory /VALUE"));
-        this->ram.total_ram = get_stoll(converter::get_value_from_key(converted_result.data[0], "TotalPhysicalMemory")) / 1024;
-        free(converted_result.data);
+        MEMORYSTATUS memory_state;
+        GlobalMemoryStatus(&memory_state);
+        this->ram.total_ram = memory_state.dwTotalPhys/1024;
     }
 
     void System::update_free_ram(){
-        converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic OS get FreePhysicalMemory /VALUE"));
-        this->ram.free_ram = get_stoll(converter::get_value_from_key(converted_result.data[0], "FreePhysicalMemory"));
-        free(converted_result.data);
+        MEMORYSTATUS memory_state;
+        GlobalMemoryStatus(&memory_state);
+        this->ram.free_ram = memory_state.dwAvailPhys/1024;
     }
 
     void System::update_basic_logical_disk_info(){
-        converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic logicaldisk get /VALUE"));
-        // time_t req_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        char test[80];
+        int ret = GetLogicalDriveStringsA(79, (LPSTR) test);
+        int counter = 0;
+        char string_buffer[20] = "";
+        for(int i = 0; i < ret; i++){
+            if(test[i] == NULL){
+                counter++;
+            }
+        }
+
         std::chrono::system_clock::time_point req_time = std::chrono::system_clock::now();
 
         this->logical_disk = {.size = 0, .data = NULL};
 
-        this->logical_disk.size = converted_result.size;
-        // result.data = (logical_disk_type*) malloc(result.size * sizeof(logical_disk_type));
+        this->logical_disk.size = counter;
         this->logical_disk.data = new logical_disk_type[this->logical_disk.size];
-        for(int i = 0; i < this->logical_disk.size; i++){
-            this->logical_disk.data[i].time = req_time;
-            this->logical_disk.data[i].last_time = this->logical_disk.data[i].time - std::chrono::seconds{1};
-            this->logical_disk.data[i].id = converter::get_value_from_key(converted_result.data[i], "DeviceID");
-            this->logical_disk.data[i].volume_name = converter::get_value_from_key(converted_result.data[i], "VolumeName");
-            this->logical_disk.data[i].total_space = get_stoll(converter::get_value_from_key(converted_result.data[i], "Size")) / 1024;
-            this->logical_disk.data[i].free_space = get_stoll(converter::get_value_from_key(converted_result.data[i], "FreeSpace")) / 1024;
-            this->logical_disk.data[i].last_free_space = this->logical_disk.data[i].free_space;
+        counter = 0;
+        int internal_counter = 0;
+        DWORD sectors_per_cluster, bytes_per_sector, number_of_free_clusters, total_number_of_clusters;
+        char volume_name[100] = "";
+        for(int i = 0; i < ret; i++){
+            if(test[i] != NULL){
+                string_buffer[counter] = test[i];
+                counter++;
+            }
+            else{
+                string_buffer[counter] = '\0';
+                counter = 0;
+                this->logical_disk.data[internal_counter].time = req_time;
+                this->logical_disk.data[internal_counter].last_time = this->logical_disk.data[i].time - std::chrono::seconds{1};
+                this->logical_disk.data[internal_counter].id.assign(string_buffer);
+                GetVolumeInformationA(string_buffer, volume_name, 99, NULL, NULL, NULL, NULL, NULL);
+                this->logical_disk.data[internal_counter].volume_name.assign(volume_name);
+                GetDiskFreeSpaceA(string_buffer, &sectors_per_cluster, &bytes_per_sector, &number_of_free_clusters, &total_number_of_clusters);
+                this->logical_disk.data[internal_counter].total_space = ((long long) total_number_of_clusters * (long long) sectors_per_cluster * (long long) bytes_per_sector) / 1024;
+                this->logical_disk.data[internal_counter].free_space = ((long long) number_of_free_clusters * (long long) sectors_per_cluster * (long long) bytes_per_sector) / 1024;
+                this->logical_disk.data[internal_counter].last_free_space = this->logical_disk.data[i].free_space;
+                internal_counter++;
+            }
         }
-
-        free(converted_result.data);
     }
 
     void System::update_logical_disk_info(){
-        converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic logicaldisk get /VALUE"));
-        // time_t req_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        std::chrono::system_clock::time_point req_time = std::chrono::system_clock::now();
-
-        for(int i = 0; i < converted_result.size; i++){
-            for(int j = 0; j < this->logical_disk.size; j++){
-                if(converter::get_value_from_key(converted_result.data[i], "DeviceID") == this->logical_disk.data[j].id){
-                    this->logical_disk.data[j].last_time = this->logical_disk.data[j].time;
-                    this->logical_disk.data[j].last_free_space = this->logical_disk.data[j].free_space;
-                    this->logical_disk.data[j].time = req_time;
-                    this->logical_disk.data[j].free_space = get_stoll(converter::get_value_from_key(converted_result.data[i], "FreeSpace")) / 1024;
-                }
+        char test[80];
+        int ret = GetLogicalDriveStringsA(79, (LPSTR) test);
+        int counter = 0;
+        char string_buffer[20] = "";
+        for(int i = 0; i < ret; i++){
+            if(test[i] == NULL){
+                counter++;
             }
         }
 
-        free(converted_result.data);
+        std::chrono::system_clock::time_point req_time = std::chrono::system_clock::now();
+
+        counter = 0;
+        DWORD sectors_per_cluster, bytes_per_sector, number_of_free_clusters, total_number_of_clusters;
+        char volume_name[100] = "";
+        for(int i = 0; i < ret; i++){
+            if(test[i] != NULL){
+                string_buffer[counter] = test[i];
+                counter++;
+            }
+            else{
+                string_buffer[counter] = '\0';
+                counter = 0;
+                for(int j = 0; j < this->logical_disk.size; j++){
+                    if(string_buffer == this->logical_disk.data[j].id){
+                        this->logical_disk.data[j].last_time = this->logical_disk.data[j].time;
+                        this->logical_disk.data[j].last_free_space = this->logical_disk.data[j].free_space;
+                        this->logical_disk.data[j].time = req_time;
+                        GetDiskFreeSpaceA(string_buffer, &sectors_per_cluster, &bytes_per_sector, &number_of_free_clusters, &total_number_of_clusters);
+                        this->logical_disk.data[j].free_space = ((long long) number_of_free_clusters * (long long) sectors_per_cluster * (long long) bytes_per_sector) / 1024;
+                    }
+                }
+            }
+        }
     }
-
-    // logical_disk_array monitoring::System::get_basic_logical_disk_info(){
-    //     converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic logicaldisk get /VALUE"));
-
-    //     logical_disk_array result = {.size = 0, .data= NULL};
-
-    //     result.size = converted_result.size;
-    //     // result.data = (logical_disk_type*) malloc(result.size * sizeof(logical_disk_type));
-    //     result.data = new logical_disk_type[result.size];
-    //     for(int i = 0; i < result.size; i++){
-    //         result.data[i].id = converter::get_value_from_key(converted_result.data[i], "DeviceID");
-    //         result.data[i].volume_name = converter::get_value_from_key(converted_result.data[i], "VolumeName");
-    //         result.data[i].total_space = get_stoll(converter::get_value_from_key(converted_result.data[i], "Size"));
-    //         result.data[i].free_space = get_stoll(converter::get_value_from_key(converted_result.data[i], "FreeSpace"));
-    //     }
-
-    //     free(converted_result.data);
-
-    //     return result;
-    // }
 
     void System::update_basic_physical_disk_info(){
         converter::organized_data_array converted_result = converter::wmic_converter(exec("wmic diskdrive get /VALUE"));
